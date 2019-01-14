8961ef437f0264a6bc7712c82cb696ed1d2e7554
Jacob Smith <jsmith0361@jh-mba158-19.local>
2019-01-14 11:08:52 -0600

    finished threeLetterWord

--- ThreeLetterWord/ViewController.swift
+++ ThreeLetterWord/ViewController.swift
@@ -10,11 +10,69 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var currentLetterLabel: UILabel!
+    @IBOutlet var labels: [UILabel]!
+    let alphabet: [String] = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+    @IBOutlet weak var stackView: UIStackView!
+    @IBOutlet weak var blankView: UIView!
+    
+    var counter: Int = 0
+    var currentLetter: String!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        getAndSetLetters()
+        
+        
     }
 
-
+    @IBAction func whenTapped(_ sender: UITapGestureRecognizer) {
+        let selectedPoint = sender.location(in: stackView)
+        
+        let selectedPointOnBlankView = sender.location(in: blankView)
+        
+        for label in labels {
+            if label.frame.contains(selectedPoint) {
+                label.text = currentLetter
+            }
+        }
+        counter += 1
+        checkIfTwentyFive()
+        getAndSetLetters()
+        
+        
+    }
+    
+    func getAndSetLetters() {
+        currentLetter = alphabet[counter]
+        currentLetterLabel.text = currentLetter
+    }
+    
+    func checkIfTwentyFive() {
+        if counter == 26 {
+            counter = 0
+            resetCounter()
+        } else {
+            return
+        }
+    }
+    
+    func resetCounter () -> Int {
+        let alert = UIAlertController(title: "Resetting Letter", message: nil, preferredStyle: .alert)
+        let okayAction = UIAlertAction(title: "Okay", style: .default) { (arctic) in
+            for label in self.labels {
+                label.text = "?"
+            }
+        }
+        alert.addAction(okayAction)
+        present(alert, animated: true, completion: nil)
+        
+        return 0
+    }
+    
+    
+    
 }
 
